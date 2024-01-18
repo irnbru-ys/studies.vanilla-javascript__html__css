ce82172846070dbe78cffa3774b88050c3164ef4
Sergei <yuriev.s.v.dev.it@gmail.com>
2024-01-18 20:33:19 +0300

    1. rendering received tasks

--- 1. ToDoList/app.js
+++ 1. ToDoList/app.js
@@ -2,12 +2,41 @@
 
 // Globals
 
+const todoList = document.querySelector('#todo-list');
 let todos = [];
 let users = [];
 
 // Attach Events
 
 document.addEventListener('DOMContentLoaded', initApp);
+
+// Basic Logic
+
+function getUserName(userId) {
+	const user = users.find(user => user.id === userId);
+
+	return user.name;
+}
+
+function printTodo({id, userId, title, completed}) {
+	const li = document.createElement('li');
+	li.classList.add('todo-item');
+	li.setAttribute('id', id);
+	li.innerHTML = `<span>${title} by <b>${getUserName(userId)}</b></span>`;
+
+	const status = document.createElement('input');
+	status.type = 'checkbox';
+	status.checked = completed;
+
+	const close = document.createElement('span');
+	close.innerHTML = '&times;';
+	close.classList.add('close');
+
+	li.prepend(status);
+	li.append(close);
+
+	todoList.prepend(li);
+}
 
 // Event Logic
 
@@ -17,8 +46,10 @@ function initApp() {
 			[todos, users] = values;
 
 	// Отправить в разметку
+	todos.forEach(todo => printTodo(todo))
 })
 }
+
 // Async logic
 
 async function getAllTodos() {
@@ -33,4 +64,4 @@ async function getAllUsers() {
 	const data = responce.json();
 
 	return data;
-}
+} 
